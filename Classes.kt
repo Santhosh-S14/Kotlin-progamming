18f486f3adbe2ae0cddd8514028143672235c38c
Santhosh-S14 <santhoshaiyar97@gmail.com>
2021-06-19 20:18:32 +0530

    Class implementation for Dice

--- Classes.kt
+++ Classes.kt
@@ -1,5 +1,12 @@
 fun main(){
-    val diceRange = 1..6
-    val randomNumber = diceRange.random()
-    println("Random no. is: ${randomNumber}")
+    val myFirstDice = Dice()
+    println(myFirstDice.sides)
+    myFirstDice.roll()
+}
+class Dice{
+    var sides = 6
+    fun roll(){
+        val diceNumber = 1..6
+        println(diceNumber.random())
+    }
 }
