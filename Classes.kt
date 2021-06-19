b10785fe7efe1febd8df575d0779a996bee15f2d
Santhosh-S14 <santhoshaiyar97@gmail.com>
2021-06-19 20:25:11 +0530

    Return your dice value

--- Classes.kt
+++ Classes.kt
@@ -1,12 +1,13 @@
 fun main(){
     val myFirstDice = Dice()
-    println(myFirstDice.sides)
-    myFirstDice.roll()
+    val n = myFirstDice.roll()
+    println("Your ${myFirstDice.sides} sided dice rolled ${n}")
 }
 class Dice{
     var sides = 6
-    fun roll(){
+    fun roll(): Int{
         val diceNumber = 1..6
-        println(diceNumber.random())
+        val randomNumber = diceNumber.random()
+        return randomNumber
     }
 }
