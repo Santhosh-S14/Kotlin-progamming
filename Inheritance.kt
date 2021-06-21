a30542e4120fbf3eb8482a20e0942ac4f62546a2
Santhosh-S14 <santhoshaiyar97@gmail.com>
2021-06-21 19:56:00 +0530

    Subclasses I

--- Inheritance.kt
+++ Inheritance.kt
@@ -1,5 +1,22 @@
 fun main(){
-    val dwelling = Dwelling(2) //cannot be instantiated
+    val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(3)
+
+    with(squareCabin){
+        println("Sqaure Cabin")
+        println("Capacity : ${capacity}")
+        println("Building Material : ${buildingMaterial}")
+        println("Has Room? : ${hasRoom()}")
+        println()
+    }
+
+    with(roundHut){
+        println("Round Hut")
+        println("Capacity : ${capacity}")
+        println("Building Material : ${buildingMaterial}")
+        println("Has Room? : ${hasRoom()}")
+        println()
+    }
 }
 
 abstract class Dwelling(private var residents: Int){
@@ -9,4 +26,14 @@ abstract class Dwelling(private var residents: Int){
     fun hasRoom(): Boolean{
         return residents < capacity
     }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+
+class RoundHut(residents: Int) : Dwelling(residents){
+    override val buildingMaterial = "Wood"
+    override val capacity = 4
 }
