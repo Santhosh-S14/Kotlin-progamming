8322e9dba1a21728228d1c4909db34de853e381c
Santhosh-S14 <santhoshaiyar97@gmail.com>
2021-07-06 16:49:44 +0530

    Order class implementation

--- Orders.kt
+++ Orders.kt
@@ -17,11 +17,45 @@ class Vegetables(vararg val toppings : String) : Item("Vegetables", 5){
     }
 }
 
+class Orders(val orderNo : Int){
+    private val items = mutableListOf<Item>()
+    
+    fun addItem(newItem : Item){
+        items.add(newItem)
+    }
+
+    fun addAll(allItems: List<Item>){
+        items.addAll(allItems)
+    }
+
+    fun print(){
+        println("Order #${orderNo}")
+        var total = 0
+        for (item in items) {
+            println("${item}: $${item.price}")
+            total += item.price
+        }
+        println("Total: $${total}")
+    }
+
+}
+
 fun main() {
-    val noodles = Noodles()
-    val vegetables = Vegetables()
-    val v2 = Vegetables("Cabbage", "Sprouts", "Onion")
-    println(noodles)
-    println(vegetables)
-    println(v2)
+    val order1 = Orders(1)
+    order1.addItem(Noodles())
+    order1.print()
+
+    println()
+
+    val order2 = Orders(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vegetables())
+    order2.print()
+
+    println()
+
+    val order3 = Orders(3)
+    val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
+    order3.addAll(items)
+    order3.print()
 }
